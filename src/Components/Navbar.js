3fd200ab2a2d5e1e7d953edd84ae377f7e68ba92
Paulson Mathew <ajoyscorpion@gmail.com>
2024-09-22 16:03:38 +0530

    fixed issues in button

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,13 +7,13 @@ function Navbar() {
         {/* Navbar */}
         <nav class="navbar navbar-expand-lg bg-body-tertiary d-flex justify-content-between ">
             <div class="container-fluid">
-                <a class="navbar-brand">Contact SMS App</a>
+                <button className="btn navbar-brand">Contact SMS App</button>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
-                        
+
                         {/* Navigate Contact Lsit */}
                         <li className="ms-3 nav-item">
                             <Link to='/' style={{textDecoration:"none",color:"black"}}>Contacts</Link>
